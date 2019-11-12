65f9419194df8713df012794ae22864cf43551aa
Randall D Williams <randallw1996@gmail.com>
2019-11-12 06:14:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0 
   
-  while square_array[counter] do
-    puts square_array[counter] ** 2 
+  while array[counter] do
+    puts array[counter] ** 2 
     counter += 1 
 end
